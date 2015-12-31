83dd86dabecf4e657e05d4d2489d9a51cba21b2b
suein_tian <tianxiaoyan2014@gmail.com>
2015-12-31 11:57:54 -0500

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
-  
 
 
   # GET /articles
@@ -300,6 +299,8 @@ class ArticlesController < ApplicationController
 
     @arr_weibo  = weibo_good_users
     @arr_wechat = wechat_good_users
+    
+    @kolApiIp = @@serverIp
     
     
     if(params[:article_id])
